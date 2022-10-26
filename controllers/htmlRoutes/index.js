e4e09db62b05e5e10907e1b825ba6c4af5c93f45
Toacin <toacinp@outlook.com>
2022-10-25 20:20:20 -0700

    homepage with posts are now showing

--- controllers/htmlRoutes/index.js
+++ controllers/htmlRoutes/index.js
@@ -1,7 +1,12 @@
 const router = require("express").Router();
+const {User, Post, Comment} = require("../../models");
 
-router.get("/", (req,res) => {
-    res.render("homepage")
+router.get("/", async (req,res) => {
+    let postData = await Post.findAll({
+        include: [{model: Comment}, {model: User}]
+    });
+    let posts = postData.map((post) => post.get({plain: true}));
+    res.render("homepage", {posts});
 })
 
 module.exports = router;
